1f8c6a5dfbbfa3feca10cf4e2150881c9f598cfc
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-01-04 13:07:09 -0600

    edit

--- environments/setenv.sh
+++ environments/setenv.sh
@@ -142,14 +142,14 @@ return 1
 
 fi 
 
-cat << EOF > "$DIR/backend.tf" 
-terraform { 
-backend "s3" { 
-bucket = "${S3BUCKET}" 
-key = "${S3BUCKETPROJ}/${S3BUCKETREGION}/${S3BUCKETTYPE}/${ENVIRONMENT}/${S3TFSTATEFILE}" 
-region = "${S3BUCKETREGION}" 
-  } 
-} 
-EOF 
+cat << EOF > "$DIR/backend.tf"
+terraform {
+backend "s3" {
+bucket = "${S3BUCKET}"
+key = "${S3BUCKETPROJ}/${S3BUCKETREGION}/${S3BUCKETTYPE}/${ENVIRONMENT}/${S3TFSTATEFILE}"
+region = "${S3BUCKETREGION}"
+  }
+}
+EOF
 
 cat backend.tf 
