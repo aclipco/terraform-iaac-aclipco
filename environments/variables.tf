b0931da4e3b564cef2a5dc883b4710fc36f2aaa6
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-01-04 12:36:13 -0600

    added variables

--- environments/variables.tf
+++ environments/variables.tf
@@ -7,3 +7,15 @@ variable "instance_type" {
 variable "key_name" {
   
 }
+
+variable "environment" {} 
+
+variable "s3_bucket" {} 
+
+variable "s3_folder_project" {} 
+
+variable "s3_folder_region" {} 
+
+variable "s3_folder_type" {} 
+
+variable "s3_tfstate_file" {} 
