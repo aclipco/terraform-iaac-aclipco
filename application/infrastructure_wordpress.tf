5935b0e49f331049b17ad4a7883b833acab1cb41
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-01-07 14:37:47 -0600

    variables added

--- application/infrastructure_wordpress.tf
+++ application/infrastructure_wordpress.tf
@@ -5,14 +5,14 @@ module "wordpress1" {
   cidr = var.cidr
 
   azs             = [var.azs1, var.azs2, var.azs3]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = [var.priv_cidr]
+  public_subnets  = [var.pub_cidr]
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  enable_nat_gateway = var.enable_nat_gateway
+  enable_vpn_gateway = var.enable_vpn_gateway
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = "var.env"
   }
 }
--- application/variable.tf
+++ application/variable.tf
@@ -1,9 +1,15 @@
 variable "name" {
   default = "wordpress1"
-}
-variable "cidr" {
-  
-}
+  }
+variable "cidr" {}
 variable "azs1" {}
 variable "azs2" {}
 variable "azs3" {}
+variable "pub_cidr" {}
+variable "priv_cidr" {}
+variable "enable_nat_gateway" {}
+variable "enable_vpn_gateway" {}
+variable "environment" {
+  
+}
+
