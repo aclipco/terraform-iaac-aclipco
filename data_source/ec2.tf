1563af4e9ecec1d55b68bb00d89dcdb338b0c427
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-01-08 18:54:12 -0600

    git push

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -15,7 +15,7 @@ output "ami" {
 
 
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+  ami           = "data.aws_ami.ubuntu.id"
   instance_type = "t2.micro"
 
   tags = {
