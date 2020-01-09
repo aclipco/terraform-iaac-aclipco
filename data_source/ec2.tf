cb45575a6ff744394b05b0a70723bbd220cc4ef4
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-01-08 19:07:45 -0600

    edit

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -21,10 +21,10 @@ output "ami" {
 
 
 resource "aws_instance" "web" {
-  ami           = "data.aws_ami.ubuntu.id"
+  ami = "data.aws_ami.ubuntu.id"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
-  }
+  Name = "HelloWorld"
+ }
 }
